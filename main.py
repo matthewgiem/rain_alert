a1052af5cfa9df6d694ed131fccbf2cafa42e599
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-09-21 14:44:15 +0700

    make api request

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from apikey import api_key
+import requests
 
+OMW_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+
+parameters = {
+    "lat": 13.87719,
+    "lon": 100.71991,
+    "appid": api_key,
+}
+
+response = requests.get(OMW_Endpoint, params=parameters)
+print(response.json())
